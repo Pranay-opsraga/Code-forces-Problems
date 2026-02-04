ad8a9d7fdc4421e96f2a195d0fc7662b748d68e7
Pranay Sharma <sharmapranay1210@gmail.com>
2026-02-04 22:00:04 +0530

    Add Jagged Swaps program implementation

---
+++ Jagged Swaps.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        int mn = *min_element(a.begin(), a.end());
+
+        if (a[0] == mn)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
