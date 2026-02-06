30952c17d48c653edad559514a5f04e4f811a576
Pranay Sharma <sharmapranay1210@gmail.com>
2026-02-06 19:31:41 +0530

    Add program to check if a number exists in input

---
+++ How Much Daytona cost ?.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        bool ok = false;
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] == k) {
+                ok = true;
+                break;
+            }
+                
+        }
+
+        cout << (ok ? "YES\n" : "NO\n");
+    }
+
+    return 0;
+}
